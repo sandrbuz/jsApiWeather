4e8b4908996c0934dbb00b55656152a37588e238
sandrbuz <sandrbuz@mail.ru>
2021-03-10 17:40:29 +0300

    secondAttempt

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-fetch('api.openweathermap.org/data/2.5/weather?id=536203&appid=655fdcf1aed1280abf8e870e95b28149')
+// async function getResponse() {
+//     let response = await fetch('api.openweathermap.org/data/2.5/weather?id=536847&appid=655fdcf1aed1280abf8e870e95b28149')
 
-    .then(function (resp) { return resp.json() })
-    .then(function (data) {
-        console.log(data);
+//     let content = await response.json()
+//     console.log(content)
+
+
+// }
+
+// getResponse()
+
+
+
+
+// -----------------------------------------------------------------------
+
+
+
+// let test = fetch('api.openweathermap.org/data/2.5/weather?id=536847&appid=655fdcf1aed1280abf8e870e95b28149')
+
+// let content = test.json();
+
+
+// console.log(test);
+
+
+// ---------------------------------------------------------------------------
+
+
+let test = fetch('api.openweathermap.org/data/2.5/weather?id=536847&appid=655fdcf1aed1280abf8e870e95b28149')
+    .then((response) => {
+        return response.json();
     })
-
-    .catch(function () {
-
+    .then((data) => {
+        console.log(data);
     });
+
+    // -----------------------------------------------------------------------------------
