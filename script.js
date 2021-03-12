c88d0fc4b6061bde176bdf2c395002015324e9b8
sandrbuz <sandrbuz@mail.ru>
2021-03-12 18:14:44 +0300

    added temperature

--- script.js
+++ script.js
@@ -43,4 +43,12 @@ fetch('http://api.openweathermap.org/data/2.5/weather?id=536203&appid=655fdcf1ae
     .then(function (resp) { return resp.json() })
     .then(function (data) {
         console.log(data);
+        document.querySelector('.Spb-city').textContent = data.name;
+        document.querySelector('.Spb-temp').innerHTML = Math.round(data.main.temp - 273) + '&deg';
+        document.querySelector('.Spb-clouds').textContent = data.weather[0][];
+
+        // https://openweathermap.org/img/wn/04n@2x.png
+    })
+    .catch(function () {
+        //catch any errors
     })
