2daccc65c148a1f5a66faec4e5946b27993f79ef
sandrbuz <sandrbuz@mail.ru>
2021-03-14 16:03:36 +0300

    Iosik halped

--- script.js
+++ script.js
@@ -38,18 +38,101 @@
 // -----------------------------------------------------------------------------------
 
 
-fetch('http://api.openweathermap.org/data/2.5/weather?id=536203&appid=655fdcf1aed1280abf8e870e95b28149')
+// fetch('http://api.openweathermap.org/data/2.5/weather?id=536203&appid=655fdcf1aed1280abf8e870e95b28149')
 
-    .then(function (resp) { return resp.json() })
-    .then(function (data) {
-        console.log(data);
-        document.querySelector('.Spb-city').textContent = data.name;
-        document.querySelector('.Spb-temp').innerHTML = Math.round(data.main.temp - 273) + '&deg';
-        document.querySelector('.Spb-weather').textContent = data.weather[0]['description'];
-        document.querySelector('.Spb-iqon').innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.weather[0]['icon'] + '@2x.png">';
+//     .then(function (resp) { return resp.json() })
+//     .then(function (data) {
+// console.log(data);
+// document.querySelector('.Spb-city').textContent = data.name;
+// document.querySelector('.Spb-temp').innerHTML = Math.round(data.main.temp - 273) + '&deg';
+// document.querySelector('.Spb-weather').textContent = data.weather[0]['description'];
+// document.querySelector('.Spb-icon').innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.weather[0]['icon'] + '@2x.png">';
 
-        // https://openweathermap.org/img/wn/04n@2x.png
+// https://openweathermap.org/img/wn/04n@2x.png  (ссылка на иконку)
+// })
+
+// .catch(function () {
+//catch any errors
+// })
+// ---------------------------------------------------------------------------------------------
+
+// arrow function BELLOW
+
+// const city = 536203;
+// fetch(`http://api.openweathermap.org/data/2.5/weather?id=${city}&appid=655fdcf1aed1280abf8e870e95b28149`)
+
+//     .then(resp => resp.json())
+//     .then(data => console.log(data))
+
+
+// .then(function (resp) { console.log(resp.json()) })
+// .then(function (data) {
+// console.log(data);
+// document.querySelector('.Spb-city').textContent = data.name;
+// document.querySelector('.Spb-temp').innerHTML = Math.round(data.main.temp - 273) + '&deg';
+// document.querySelector('.Spb-weather').textContent = data.weather[0]['description'];
+// document.querySelector('.Spb-icon').innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.weather[0]['icon'] + '@2x.png">';
+
+// https://openweathermap.org/img/wn/04n@2x.png  (ссылка на иконку)
+// })
+
+// .catch(function () {
+//catch any errors
+// })
+// ----------------------------------------------------------------------------------------
+
+
+// buttons requests
+
+
+const button = document.querySelector('.city-btn');
+
+const buttons = document.querySelectorAll('.city-btn');
+
+// buttons[0].addEventListener('click', function (event) {
+
+//     const cityName = event.target.textContent;
+//     console.log(cityName);
+//     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=655fdcf1aed1280abf8e870e95b28149`)
+//         .then(resp => resp.json())
+//         .then(data => console.log(data))
+
+// })
+// buttons[1].addEventListener('click', function (event) {
+
+//     const cityName = event.target.textContent;
+//     console.log(cityName);
+//     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=655fdcf1aed1280abf8e870e95b28149`)
+//         .then(resp => resp.json())
+//         .then(data => console.log(data))
+
+// })
+// buttons[2].addEventListener('click', function (event) {
+
+//     const cityName = event.target.textContent;
+//     console.log(cityName);
+//     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=655fdcf1aed1280abf8e870e95b28149`)
+//         .then(resp => resp.json())
+//         .then(data => console.log(data))
+
+// })
+
+
+for (i = 0; i < buttons.length; i += 1) {
+
+    buttons[i].addEventListener('click', function (event) {
+
+        const cityName = event.target.textContent;
+        console.log(cityName);
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=655fdcf1aed1280abf8e870e95b28149`)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+
+            })
+
+
     })
-    .catch(function () {
-        //catch any errors
-    })
+}
+
+
