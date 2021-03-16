d0b62ab017033d244a45fdcc295c23269d306eb2
sandrbuz <sandrbuz@mail.ru>
2021-03-16 12:36:17 +0300

    string interpolation

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ for (i = 0; i < buttons.length; i += 1) {
                 document.querySelector('.city').textContent = data.name;
                 document.querySelector('.temp').innerHTML = Math.round(data.main.temp - 273) + '&deg';
                 document.querySelector('.weather').textContent = data.weather[0]['description'];
-                document.querySelector('.icon').innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.weather[0]['icon'] + '@2x.png">';
+                document.querySelector('.icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
             })
 
 
